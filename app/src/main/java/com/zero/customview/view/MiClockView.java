88fd7b20790ccc661c5a9278977f6f726f93af9c
wumin@sunland <fusu1435@163.com>
2017-07-18 15:16:54 +0800

    Optimisum mi clock view

--- app/src/main/java/com/zero/customview/view/MiClockView.java
+++ app/src/main/java/com/zero/customview/view/MiClockView.java
@@ -1,5 +1,7 @@
 package com.zero.customview.view;
 
+import android.animation.PropertyValuesHolder;
+import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.pm.ProviderInfo;
@@ -14,9 +16,12 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.SweepGradient;
 import android.provider.CalendarContract;
+import android.support.annotation.BoolRes;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 import com.orhanobut.logger.Logger;
 import com.zero.customview.CanvasViewActivity;
@@ -154,7 +159,7 @@ public class MiClockView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         mRadius = Math.min(w - getPaddingLeft() - getPaddingRight(),
                             h - getPaddingTop() - getPaddingBottom()) / 2;
-        mDefaultPadding = 0.15f * mRadius;
+        mDefaultPadding = 0.2f * mRadius;
         mPaddingLeft = mDefaultPadding + w/ 2 - mRadius + getPaddingLeft();
         mPaddingTop = mDefaultPadding + h / 2 - mRadius + getPaddingTop();
         mPaddingRight = mPaddingLeft;
@@ -180,7 +185,26 @@ public class MiClockView extends View {
         drawSecondHand();
         drawMinuteHand();
         drawHourHand();
+        drawCoverCircle();
         invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                getCameraRotate(event);
+                getCanvasTranslate(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                getCameraRotate(event);
+                getCanvasTranslate(event);
+                break;
+            case MotionEvent.ACTION_UP:
+                startShakeAnim();
+                break;
+        }
+        return true;
     }
 
     private int measureDimension(int measureSpec) {
@@ -195,11 +219,63 @@ public class MiClockView extends View {
                 result = Math.min(result, size);
             }
         }
-
         return result;
     }
 
-    private void setCameraRotate() {}
+    private void getCameraRotate(MotionEvent event) {
+        if (mShakeAnim != null && mShakeAnim.isRunning()) {
+            mShakeAnim.cancel();
+        }
+
+        float rotateX = - (event.getY() - getHeight() / 2);
+        float rotateY = (event.getX() - getWidth() / 2);
+        float percentX = rotateX / mRadius;
+        float percentY= rotateY / mRadius;
+        if (percentX > 1) {
+            percentX = 1;
+        } else if (percentX < -1) {
+            percentX = -1;
+        }
+        if (percentY > 1) {
+            percentY = 1;
+        } else if (percentY < -1) {
+            percentY = -1;
+        }
+        mCameraRotateX = percentX * mCameraRotateMax;
+        mCameraRotateY = percentY * mCameraRotateMax;
+    }
+
+    private void getCanvasTranslate(MotionEvent event) {
+        float translateX = (event.getX() - getWidth() / 2);
+        float translateY = (event.getY() - getHeight() / 2);
+        float percentX = translateX / mRadius;
+        float percentY= translateX / mRadius;
+        if (percentX > 1) {
+            percentX = 1;
+        } else if (percentX < -1) {
+            percentX = -1;
+        }
+        if (percentY > 1) {
+            percentY = 1;
+        } else if (percentY < -1) {
+            percentY = -1;
+        }
+
+        mCanvasTranslateX = percentX * mCanvasTranslateMax;
+        mCanvasTranslateY = percentY * mCanvasTranslateMax;
+    }
+
+    private void setCameraRotate() {
+        mCameraMatrix.reset();
+        mCamera.save();
+        mCamera.rotateX(mCameraRotateX);
+        mCamera.rotateY(mCameraRotateY);
+        mCamera.getMatrix(mCameraMatrix);
+        mCamera.restore();
+        mCameraMatrix.preTranslate(-getWidth() / 2, -getHeight() / 2);
+        mCameraMatrix.postTranslate(getWidth() / 2, getHeight() / 2);
+        mCanvas.concat(mCameraMatrix);
+    }
 
     private void getTimeAngle() {
         Calendar calendar = Calendar.getInstance();
@@ -239,24 +315,133 @@ public class MiClockView extends View {
 
     private void drawScaleLine() {
         mCanvas.save();
-
+        mCanvas.translate(mCanvasTranslateX, mCanvasTranslateY);
+        mScaleRectF.set(mPaddingLeft + 1.5f * mScaleLength + mTextRect.height() / 2,
+                mPaddingTop + 1.5f * mScaleLength + mTextRect.height() / 2,
+                getWidth() - mPaddingRight - mTextRect.height() / 2 - 1.5f * mScaleLength,
+                getHeight() - mPaddingBottom - mTextRect.height() / 2 - 1.5f * mScaleLength);
+        mGradientMatrix.setRotate(mSecondAngle - 90, getWidth() / 2, getHeight() / 2);
+        mSweepGradient.setLocalMatrix(mGradientMatrix);
+        mScaleArcPaint.setShader(mSweepGradient);
+        mCanvas.drawArc(mScaleRectF, 0, 360, false, mScaleArcPaint);
 
         for (int i = 0; i < 200; i++) {
-            mCanvas.drawLine(getWidth()/2, mPaddingTop + mScaleLength + mTextRect.height() / 2,
-                    getWidth() / 2,
-                    mPaddingTop + 2 * mScaleLength + mTextRect.height() / 2,
-                    mScaleLinePaint);
+            mCanvas.drawLine(getWidth() / 2, mPaddingTop + mScaleLength + mTextRect.height() / 2,
+                    getWidth() / 2, mPaddingTop + 2 * mScaleLength + mTextRect.height() / 2, mScaleLinePaint);
             mCanvas.rotate(1.8f, getWidth() / 2, getHeight() / 2);
         }
         mCanvas.restore();
     }
 
     private void drawSecondHand() {
-
+        mCanvas.save();
+        mCanvas.translate(mCanvasTranslateX, mCanvasTranslateY);
+        mCanvas.rotate(mSecondAngle, getWidth() / 2, getHeight() / 2);
+        mSecondHandPath.reset();
+        float offset = mPaddingTop + mTextRect.height() / 2;
+        mSecondHandPath.moveTo(getWidth() / 2, offset + 0.27f* mRadius);
+        mSecondHandPath.lineTo(getWidth() / 2 - 0.05f * mRadius, offset + 0.35f * mRadius);
+        mSecondHandPath.lineTo(getWidth() / 2 + 0.05f * mRadius, offset + 0.35f * mRadius);
+        mSecondHandPath.close();
+        mScondHandPaint.setColor(mLightColor);
+        mCanvas.drawPath(mSecondHandPath, mScondHandPaint);
+        mCanvas.restore();
     }
 
-    private void drawMinuteHand() {}
+    private void drawMinuteHand() {
+        mCanvas.save();
+        mCanvas.translate(mCanvasTranslateX*2f, mCanvasTranslateY*2f);
+        mCanvas.rotate(mMinuteAngle, getWidth() / 2, getHeight() / 2);
+        mMinuteHandPath.reset();
+        float offset = mPaddingTop + mTextRect.height() / 2;
+        mMinuteHandPath.moveTo(getWidth() / 2 - 0.01f * mRadius, getHeight() / 2 - 0.03f * mRadius);
+        mMinuteHandPath.lineTo(getWidth() / 2 - 0.008f * mRadius, offset + 0.365f * mRadius);
+        mMinuteHandPath.quadTo(getWidth() / 2, offset + 0.345f * mRadius,
+                getWidth() / 2 + 0.008f * mRadius, offset + 0.365f * mRadius);
+        mMinuteHandPath.lineTo(getWidth() / 2 + 0.01f * mRadius, getHeight() / 2 - 0.03f * mRadius);
+        mMinuteHandPath.close();
+        mMinuteHandPaint.setStyle(Paint.Style.FILL);
+        mCanvas.drawPath(mMinuteHandPath, mMinuteHandPaint);
 
-    private void drawHourHand() {}
+        mCircleRectF.set(getWidth() / 2 - 0.03f * mRadius, getHeight() / 2 - 0.03f * mRadius,
+                getWidth() / 2 + 0.03f * mRadius, getHeight() / 2 + 0.03f * mRadius);
+        mMinuteHandPaint.setStyle(Paint.Style.STROKE);
+        mMinuteHandPaint.setStrokeWidth(0.02f * mRadius);
+        mCanvas.drawArc(mCircleRectF, 0, 360, false, mMinuteHandPaint);
+        mCanvas.restore();
+    }
+
+    private void drawHourHand() {
+        mCanvas.save();
+        mCanvas.translate(mCanvasTranslateX*2f, mCanvasTranslateY*2f );
+        mCanvas.rotate(mHourAngle, getWidth() / 2, getHeight() / 2);
+        mHoutHandPath.reset();
+        float offset = mPaddingTop + mTextRect.height() / 2;
+        mHoutHandPath.moveTo(getWidth() / 2 - 0.018f * mRadius, getHeight() / 2 - 0.03f * mRadius);
+        mHoutHandPath.lineTo(getWidth() / 2 - 0.009f * mRadius, offset + 0.48f * mRadius);
+        mHoutHandPath.quadTo(getWidth() / 2, offset + 0.46f * mRadius,
+                getWidth() / 2 + 0.009f * mRadius, offset + 0.48f * mRadius);
+        mHoutHandPath.lineTo(getWidth() / 2 + 0.018f * mRadius, getHeight() / 2 - 0.03f * mRadius);
+        mHoutHandPath.close();
+        mMinuteHandPaint.setStyle(Paint.Style.FILL);
+        mCanvas.drawPath(mHoutHandPath, mHourHandPaint);
+
+        mCircleRectF.set(getWidth() / 2 - 0.03f * mRadius, getHeight() / 2 - 0.03f * mRadius,
+                getWidth() / 2 + 0.03f * mRadius, getHeight() / 2 + 0.03f * mRadius);
+        mMinuteHandPaint.setStyle(Paint.Style.STROKE);
+        mMinuteHandPaint.setStrokeWidth(0.01f * mRadius);
+        mCanvas.drawArc(mCircleRectF, 0, 360, false, mMinuteHandPaint);
+        mCanvas.restore();
+    }
+
+    private void drawCoverCircle() {
+        mCanvas.save();
+        mCanvas.translate(mCanvasTranslateX*2f, mCanvasTranslateY*2f);
+        mCanvas.drawCircle(getWidth() / 2, getHeight() / 2, 0.05f * mRadius, mScondHandPaint);
+        mScondHandPaint.setColor(mBgColor);
+        mCanvas.drawCircle(getWidth() / 2, getHeight() / 2, 0.025f * mRadius, mScondHandPaint);
+        mCanvas.restore();
+    }
+
+    private void startShakeAnim() {
+        final String cameraRotateXName = "cameraRotateX";
+        final String cameraRotateYName = "cameraRotateY";
+        final String canvasTranslateXName = "canvasTranslateX";
+        final String canvasTranslateYName = "canvasTranslateY";
+        PropertyValuesHolder cameraRotateXHolder =
+                PropertyValuesHolder.ofFloat(cameraRotateXName, mCameraRotateX, 0);
+
+        PropertyValuesHolder cameraRotateYHolder =
+                PropertyValuesHolder.ofFloat(cameraRotateYName, mCameraRotateY, 0);
+
+        PropertyValuesHolder canvasTranslateXHolder =
+                PropertyValuesHolder.ofFloat(canvasTranslateXName, mCanvasTranslateX, 0);
+        PropertyValuesHolder canvasTranslateYHolder =
+                PropertyValuesHolder.ofFloat(canvasTranslateYName, mCanvasTranslateY, 0);
+
+        mShakeAnim = ValueAnimator.ofPropertyValuesHolder(cameraRotateXHolder, cameraRotateYHolder,
+                canvasTranslateXHolder, canvasTranslateYHolder);
+        mShakeAnim.setInterpolator(new TimeInterpolator() {
+            @Override
+            public float getInterpolation(float input) {
+                float f = 0.571429f;
+                return (float)(Math.pow(2, -2*input)*Math.sin((input - f / 4)*(2 * Math.PI)/f) + 1);
+            }
+        });
+        mShakeAnim.setDuration(500);
+        mShakeAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mCameraRotateX = (float) animation.getAnimatedValue(cameraRotateXName);
+                mCameraRotateY = (float) animation.getAnimatedValue(cameraRotateYName);
+                mCanvasTranslateX = (float) animation.getAnimatedValue(canvasTranslateXName);
+                mCanvasTranslateY = (float) animation.getAnimatedValue(canvasTranslateYName);
+            }
+        });
+        mShakeAnim.start();
+    }
+
+
+
 
 }

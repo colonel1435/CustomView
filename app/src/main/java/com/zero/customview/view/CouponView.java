299abc8704b0d8ac07b09d743d4eb06d42aa2d48
Mr.Wumin <fusu1435@163.com>
2017-11-10 22:47:28 +0800

    Optimisum coupon view.

--- app/src/main/java/com/zero/customview/view/CouponView.java
+++ app/src/main/java/com/zero/customview/view/CouponView.java
@@ -9,24 +9,16 @@ import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathEffect;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.graphics.Shader;
-import android.graphics.Xfermode;
-import android.icu.text.TimeZoneFormat;
 import android.support.v4.content.ContextCompat;
-import android.text.InputFilter;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.zero.customview.R;
 import com.zero.customview.utils.DisplayUtils;
-
-import java.util.zip.CheckedOutputStream;
 
 
 /**
@@ -36,12 +28,13 @@ import java.util.zip.CheckedOutputStream;
  * Date   : 2017/11/10 0010 14:25
  */
 
-public class CouponView extends ViewGroup {
+public class CouponView extends LinearLayout {
     private static final String TAG = "CouponView@wumin";
     private final static int DEFAULT_BINDER_HOLE_RADIUS = 10;
     private final static int DEFAULT_CHECK_HOLE_RADIUS = 20;
     private final static int DEFAULT_SEPERATOR_WIDTH = 4;
     private final static float DEFAULT_MARKER_SIZE = 32;
+    private final static int DEFAULT_MARKER_ALPHA = 64;
     private Context mContext;
     private Paint mBorderPaint;
     private Paint mLinePaint;
@@ -51,14 +44,18 @@ public class CouponView extends ViewGroup {
     private int mSeperatorColor;
     private int mBorderColor;
     private int mMarkerColor;
+    private int mLeftColor;
+    private int mRightColor;
     private float mMarkerSize;
     private float mBinderHoleRadius;
     private float mCheckHoleRadius;
     private String mWaterMarker;
+    private int mMarkerAlpha;
     private float mWidth;
     private float mHeight;
     private float baseX;
     private float baseY;
+    private float baseLeft, baseTop, baseRight, baseBottom;
 
     public CouponView(Context context) {
         this(context, null);
@@ -79,19 +76,23 @@ public class CouponView extends ViewGroup {
                 DEFAULT_BINDER_HOLE_RADIUS);
         mCheckHoleRadius = ta.getDimensionPixelSize(R.styleable.CouponView_coupon_check_radius,
                 DEFAULT_CHECK_HOLE_RADIUS);
-        mWaterMarker = ta.getString(R.styleable.CouponView_coupan_water_marker);
-        mMarkerColor = ta.getColor(R.styleable.CouponView_coupan_marker_color, Color.WHITE);
+        mWaterMarker = ta.getString(R.styleable.CouponView_coupon_water_marker);
+        mMarkerColor = ta.getColor(R.styleable.CouponView_coupon_marker_color, Color.WHITE);
         mMarkerSize = ta.getDimension(R.styleable.CouponView_coupon_marker_size,
                 DEFAULT_MARKER_SIZE);
+        mLeftColor = ta.getColor(R.styleable.CouponView_coupon_left_color, Color.BLUE);
+        mRightColor = ta.getColor(R.styleable.CouponView_coupon_right_color, Color.GREEN);
         ta.recycle();
         mContext = context;
         initView();
     }
 
     private void initView() {
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         setBackgroundColor(ContextCompat.getColor(mContext, R.color.transparent));
+        mMarkerAlpha = DEFAULT_MARKER_ALPHA;
+
         mBorderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mBorderPaint.setStrokeWidth(10);
         mBorderPaint.setColor(mBorderColor);
 
         mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -109,22 +110,16 @@ public class CouponView extends ViewGroup {
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int childCount = getChildCount();
-        for(int i = 0; i < childCount; i++) {
-            View child = getChildAt(i);
-            measureChild(child, widthMeasureSpec, heightMeasureSpec);
-        }
-    }
-
-    @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mWidth = w;
         mHeight = h;
         baseX = (float) (w * 0.25);
         baseY = (float) (h * 0.5);
+        baseLeft = getPaddingLeft();
+        baseTop = getPaddingTop();
+        baseRight = mWidth - getPaddingRight();
+        baseBottom = mHeight - getPaddingBottom();
         if (mBinderHoleRadius == DEFAULT_BINDER_HOLE_RADIUS) {
             mBinderHoleRadius = (float) (mHeight * 0.1);
         }
@@ -132,14 +127,11 @@ public class CouponView extends ViewGroup {
             mCheckHoleRadius = (float) (mHeight * 0.05);
         }
         if (mMarkerSize == DEFAULT_MARKER_SIZE) {
-            mMarkerSize = (float) DisplayUtils.px2sp(mContext, (float)(mHeight * 0.65));
+            mMarkerSize = DisplayUtils.px2sp(mContext, (float) (mWidth * 0.35));
         }
+
+        postInvalidate();
         Log.d(TAG, "onSizeChanged: mWidth -> " + mWidth + " mHeight -> " + mHeight);
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
     }
 
     @Override
@@ -152,45 +144,40 @@ public class CouponView extends ViewGroup {
 
     private void drawBoarder(Canvas canvas) {
         canvas.save();
-         float left = getLeft();
-         float top = getTop();
-         float right = getRight();
-         float bottom = getBottom();
-        Log.d(TAG, "drawBoarder: left[" + left + "],top[" + top + "],right[" + right + "],bottom["
-            + bottom + "]");
-         mBorderPath.moveTo(left - mCheckHoleRadius, top);
-         RectF ltRect = new RectF(left - mCheckHoleRadius, top - mCheckHoleRadius,
-                                left + mCheckHoleRadius, top + mCheckHoleRadius);
+        Log.d(TAG, "drawBoarder: baseLeft[" + baseLeft + "],baseTop[" + baseTop + "],baseRight[" + baseRight + "],baseBottom["
+            + baseBottom + "]");
+         mBorderPath.moveTo(baseLeft - mCheckHoleRadius, baseTop);
+         RectF ltRect = new RectF(baseLeft - mCheckHoleRadius, baseTop - mCheckHoleRadius,
+                                baseLeft + mCheckHoleRadius, baseTop + mCheckHoleRadius);
          mBorderPath.arcTo(ltRect, 0, 90);
-         mBorderPath.lineTo(left, baseY - mBinderHoleRadius);
-         RectF lcRect = new RectF(left - mBinderHoleRadius, baseY - mBinderHoleRadius,
-                                    left + mBinderHoleRadius, baseY + mBinderHoleRadius);
+         mBorderPath.lineTo(baseLeft, baseY - mBinderHoleRadius);
+         RectF lcRect = new RectF(baseLeft - mBinderHoleRadius, baseY - mBinderHoleRadius,
+                                    baseLeft + mBinderHoleRadius, baseY + mBinderHoleRadius);
          mBorderPath.arcTo(lcRect, -90, 180);
-         mBorderPath.lineTo(left, bottom - mCheckHoleRadius);
-         RectF lbRect = new RectF(left - mCheckHoleRadius, bottom - mCheckHoleRadius,
-                                    left + mCheckHoleRadius, bottom + mCheckHoleRadius);
+         mBorderPath.lineTo(baseLeft, baseBottom - mCheckHoleRadius);
+         RectF lbRect = new RectF(baseLeft - mCheckHoleRadius, baseBottom - mCheckHoleRadius,
+                                    baseLeft + mCheckHoleRadius, baseBottom + mCheckHoleRadius);
          mBorderPath.arcTo(lbRect, -90, 90);
-         mBorderPath.lineTo(baseX - mCheckHoleRadius, bottom);
-         RectF bRect = new RectF(baseX - mCheckHoleRadius, bottom - mCheckHoleRadius,
-                                baseX + mCheckHoleRadius, bottom + mCheckHoleRadius);
+         mBorderPath.lineTo(baseX - mCheckHoleRadius, baseBottom);
+         RectF bRect = new RectF(baseX - mCheckHoleRadius, baseBottom - mCheckHoleRadius,
+                                baseX + mCheckHoleRadius, baseBottom + mCheckHoleRadius);
          mBorderPath.arcTo(bRect, 180, 180);
-         mBorderPath.lineTo(right - mCheckHoleRadius, bottom);
-         RectF rbRect = new RectF(right - 2 * mCheckHoleRadius, bottom - 2 * mCheckHoleRadius,
-                                    right, bottom);
+         mBorderPath.lineTo(baseRight - mCheckHoleRadius, baseBottom);
+         RectF rbRect = new RectF(baseRight - 2 * mCheckHoleRadius, baseBottom - 2 * mCheckHoleRadius,
+                                    baseRight, baseBottom);
          mBorderPath.arcTo(rbRect, 90, -90);
-         mBorderPath.lineTo(right, top - mCheckHoleRadius);
-         RectF rtRect = new RectF(right - 2 * mCheckHoleRadius, top,
-                                    right, top + 2 * mCheckHoleRadius);
+         mBorderPath.lineTo(baseRight, baseTop - mCheckHoleRadius);
+         RectF rtRect = new RectF(baseRight - 2 * mCheckHoleRadius, baseTop,
+                                    baseRight, baseTop + 2 * mCheckHoleRadius);
          mBorderPath.arcTo(rtRect, 0, -90);
-         mBorderPath.lineTo(baseX + mCheckHoleRadius, top);
-         RectF tRect = new RectF(baseX - mCheckHoleRadius, top - mCheckHoleRadius,
-                                    baseX + mCheckHoleRadius, top + mCheckHoleRadius);
+         mBorderPath.lineTo(baseX + mCheckHoleRadius, baseTop);
+         RectF tRect = new RectF(baseX - mCheckHoleRadius, baseTop - mCheckHoleRadius,
+                                    baseX + mCheckHoleRadius, baseTop + mCheckHoleRadius);
          mBorderPath.arcTo(tRect, 0, 180);
          mBorderPath.close();
 
-        Shader shader = new LinearGradient(left, top, right, bottom,
-                Color.parseColor("#76c1e9"), Color.parseColor("#00c6b5"),
-                Shader.TileMode.CLAMP);
+        Shader shader = new LinearGradient(baseLeft, baseTop, baseRight, baseBottom,
+                mLeftColor, mRightColor, Shader.TileMode.CLAMP);
         mBorderPaint.setShader(shader);
         canvas.drawPath(mBorderPath, mBorderPaint);
         canvas.restore();
@@ -198,23 +185,22 @@ public class CouponView extends ViewGroup {
 
     private void drawSeperator(Canvas canvas) {
         canvas.save();
-        int top = getTop();
-        int bottom = getBottom();
-        canvas.drawLine(baseX, top + mCheckHoleRadius,
-                        baseX, bottom - mCheckHoleRadius, mLinePaint);
+        canvas.drawLine(baseX, baseTop + mCheckHoleRadius,
+                        baseX, baseBottom - mCheckHoleRadius, mLinePaint);
         canvas.restore();
     }
 
     private void drawWaterMarker(Canvas canvas) {
         if (mWaterMarker != null && !"".equals(mWaterMarker)) {
             canvas.save();
-            int right = getRight();
             Paint.FontMetrics fontMetrics = mMarkerPaint.getFontMetrics();
-            float baseline_x = right;
-            float baseline_y = baseY - (fontMetrics.descent - fontMetrics.ascent)/2;
+            float baseline_x = baseRight;
+            float baseline_y = baseTop + mMarkerSize;
             mMarkerPaint.setFakeBoldText(true);
+            mMarkerPaint.setTextSkewX(-0.3f);
             mMarkerPaint.setTextSize(mMarkerSize);
             mMarkerPaint.setTextAlign(Paint.Align.RIGHT);
+            mMarkerPaint.setAlpha(mMarkerAlpha);
             canvas.drawText(mWaterMarker, baseline_x, baseline_y, mMarkerPaint);
             canvas.restore();
         }
